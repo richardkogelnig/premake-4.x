80005f09384d713b30cfb8507414b481bf1ad259
richardk <richard.kogelnig@gmail.com>
2016-03-08 13:24:18 +0100

    [intermediate] step towards handling premake configurations in fastbuild by extending a base config struct, missing: premake flag to compiler flag conversion

--- src/actions/fastbuild/fastbuild_project.lua
+++ src/actions/fastbuild/fastbuild_project.lua
@@ -18,74 +18,35 @@
 		_p('; ID: {%s}', prj.uuid)
 		_p('')
 
+
 		-- TODO: richardk translate all compiler flags for platform? (maybe use some external function or map)
+
+
 		-- TODO: richardk how to mix the different configs comming from premake with the ones from fastbuild?
 		--          e.g. premake defines Release and Debug with different paths ... ?
-		_p('{')
-		_p("    .ProjectName = '%s'", prj.name)
-		_p("    .ProjectPath = '.'", prj.basedir) -- TODO: richardk can all Projects have . as the projectpath?
-		_p("")
 
-        _p('    .LibInputFiles = ')
-		_p('    {')
-		-- if action == "Compile" and fname:endswith(".cs") 
-			-- List out the folders and files that make up the build
-			local tr = premake.project.buildsourcetree(prj)
-			premake.tree.sort(tr)
-			premake.tree.traverse(tr, {
-				-- onbranch = function(node, depth)
-				--	_p(depth, path.getname(node.name) .. "/")
-				-- end,
-			
-				onleaf = function(node, depth)
-					local rel_filepath = node.path 
-					if rel_filepath:endswith(".c") or rel_filepath:endswith(".cpp") then -- TODO richardk make this filter configurable (e.g. acccept .cxx?) 
-						_p('        "%s",', path.getrelative( prj.basedir, node.path))
-					end
+for cfg in premake.eachconfig(prj) do
+            
+            _p('.%s = ', cfg.name)
+            _p('[')
+            _p(1,'Using( .X86BaseConfig )')           
+            _p(1,".Config					= '%s'", cfg.name)
+			_p(2, ';Defines: %s', table.concat(cfg.defines, ";"))            
+            
+            for k,v in pairs(cfg.defines) do
+                _p(1,".CompilerOptions + ' /D%s '",v) -- iterate over all defines
+            end
+            for k,v in pairs(cfg.includedirs) do
 
-				end
-			})     
-		_p('    }')           
-        _p("")
+                _p (1,".CompilerOptions + ' /I%s '", '"'..v..'"')
+            end
+            _p(1, '.CompilerOutputPath =  "%s"', cfg.buildtarget.directory)
+		    _p(1, ';Flags: %s', table.concat(cfg.flags, ", "))            
 
-		-- List the build configurations, and the settings for each
-		for cfg in premake.eachconfig(prj) do
-
-        -----
-
-		_p("    ForEach(.Config in .Configs_Windows_MSVC)")
-
-		_p('    {')
-			_p('        Using( .Config )')
-			-- TODO: richardk distinguish  Library/StaticLibrary vs DLL/SharedLib vs App/ConsoleApp WindowedApp') 
-			if prj.kind == "StaticLibrary" then
-				_p('        Library(%s-Lib-$Platform$-$Config$)', prj.name) -- TODO: richardk should it be the name or something else?
-
-				
-			end 
-
-			if prj.kind == "ConsoleApp" then
-				_p('        ObjectList("%s-Lib-$Platform$-$Config$")', prj.name) 
-				_p('        {')     
-                _p('            .CompilerInputPath = .LibInputFiles ; defined above'  )
-				_p('            .CompilerOutputPath = "tmp/$Platform$-$Config$/"')   
-				_p('        }')
-				_p('        Executable("%s-Exe-$Platform$-$Config$")', prj.name) -- TODO: richardk should it be the name or something else?
-				_p('        {')
-				_p('            .Libraries          = { "%s-Lib-$Platform$-$Config$"}', prj.name)
-				_p('            .LinkerOutput       = "%s-Lib-$Platform$-$Config$.exe"', prj.name)
-				_p('        }')
-
-				
-			end 
-		_p('    }')
-
-		_p('}')
-        ------
+            _p(1,'.LinkerOutput = "%s"', cfg.buildtarget.fullpath)
 
 
-
-			_p(';Configuration %s:', cfg.name)
+			 _p(';Configuration %s:', cfg.name)
 			_p(1, ';Objects directory: %s', cfg.objectsdir)
 
 			_p(1, ';Build target:')
@@ -98,9 +59,8 @@
 			_p('')
 
 			_p(1, ';Compiling:')
-			_p(2, ';Defines: %s', table.concat(cfg.defines, ";"))
+
 			_p(2, ';Include paths: %s', table.concat(cfg.includedirs, ";"))
-			_p(2, ';Flags: %s', table.concat(cfg.flags, ", "))
 			if not cfg.flags.NoPCH and cfg.pchheader then
 				_p(2, ';Precompiled header: %s (%s)', cfg.pchheader, cfg.pchsource)
 			end
@@ -136,6 +96,73 @@
 				end
 				_p('')
 			end
+            _p(']')
 		end
+        ------------------------------------
+_p(".Configs_Windows_MSVC	= {")
+for cfg in premake.eachconfig(prj) do
+    _p(1,'.%s,', cfg.name)
+end
+_p("}")
+
+        -----------------------
+		_p('{')
+		_p("    .ProjectName = '%s'", prj.name)
+		_p("    .ProjectPath = '.'", prj.basedir) -- TODO: richardk can all Projects have . as the projectpath?
+		_p("")
+
+        _p('    .LibInputFiles = ')
+		_p('    {')
+		-- if action == "Compile" and fname:endswith(".cs") 
+			-- List out the folders and files that make up the build
+			local tr = premake.project.buildsourcetree(prj)
+			premake.tree.sort(tr)
+			premake.tree.traverse(tr, {
+				-- onbranch = function(node, depth)
+				--	_p(depth, path.getname(node.name) .. "/")
+				-- end,
+			
+				onleaf = function(node, depth)
+					local rel_filepath = node.path 
+					if rel_filepath:endswith(".c") or rel_filepath:endswith(".cpp") then -- TODO richardk make this filter configurable (e.g. acccept .cxx?) 
+						_p('        "%s",', path.getrelative( prj.basedir, node.path))
+					end
+
+				end
+			})     
+		_p('    }')           
+        _p("")
+
+		_p("    ForEach(.Config in .Configs_Windows_MSVC)")
+
+		_p('    {')
+			_p('        Using( .Config )')
+			-- TODO: richardk distinguish  Library/StaticLibrary vs DLL/SharedLib vs App/ConsoleApp WindowedApp') 
+			if prj.kind == "StaticLibrary" then
+				_p('        Library(%s-Lib-$Platform$-$Config$)', prj.name) -- TODO: richardk should it be the name or something else?
+
+				
+			end 
+
+			if prj.kind == "ConsoleApp" then
+				_p('        ObjectList("%s-Lib-$Platform$-$Config$")', prj.name) 
+				_p('        {')     
+                _p('            .CompilerInputFiles = .LibInputFiles ; defined above'  )
+				_p('            .CompilerOutputPath = "tmp/$Platform$-$Config$/"')   
+				_p('        }')
+				_p('        Executable("%s-Exe-$Platform$-$Config$")', prj.name) -- TODO: richardk should it be the name or something else?
+				_p('        {')
+				_p('            .Libraries          = { "%s-Lib-$Platform$-$Config$"}', prj.name)
+				_p('            .LinkerOutput       = "%s-Lib-$Platform$-$Config$.exe"', prj.name)
+				_p('        }')
+
+				
+			end 
+		_p('    }')
+
+		_p('}')
+
+		-- List the build configurations, and the settings for each
+		
 		
 	end
